9cc309c134a889ec2fed5e7402b56744501f56c3
Arkadii Chyzhov <arkadii.chyzhov@mytheresa.com>
2017-02-12 14:40:20 +0100

    adjusted query

--- Model/SimpleProductsAggregatedReportDataProcessor.php
+++ Model/SimpleProductsAggregatedReportDataProcessor.php
@@ -28,48 +28,65 @@ namespace MagentoHackathon\BestsellersSorting\Model;
 
 class SimpleProductsAggregatedReportDataProcessor implements DataProcessorInterface
 {
-
-
     /**
      * @var \Magento\Framework\App\ResourceConnection
      */
     private $resource;
 
-    public function __construct(
-        \Magento\Framework\App\ResourceConnection $resource
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $scopeConfig;
 
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     */
+    private $date;
+
+    /**
+     * @var \Magento\Eav\Model\Config
+     */
+    private $eavConfig;
+
+    /**
+     * SimpleProductsAggregatedReportDataProcessor constructor.
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\Stdlib\DateTime\DateTime $date
+     * @param \Magento\Eav\Model\Config $eavConfig
+     */
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resource,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\Stdlib\DateTime\DateTime $date,
+        \Magento\Eav\Model\Config $eavConfig
     )
     {
-
         $this->resource = $resource;
+        $this->scopeConfig = $scopeConfig;
+        $this->date = $date;
+        $this->eavConfig = $eavConfig;
     }
 
     /**
      * @inheritdoc
      */
-    public function calculate()
+    public function calculate($storeId = 0)
     {
 
         $connection = $this->resource->getConnection();
+
+        $periodDays = $this->scopeConfig->getValue(
+            'catalog/group/period',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
         $column = 'qty_ordered';
-        $mainTable = 'sales_bestseller_aggregated_daily';
-        $type = 'month';
+        $mainTable = 'sales_bestsellers_aggregated_daily';
 
         $periodSubSelect = $connection->select();
         $ratingSubSelect = $connection->select();
         $ratingSelect = $connection->select();
-
-        switch ($type) {
-            case 'year':
-                $periodCol = $connection->getDateFormatSql('t.period', '%Y-01-01');
-                break;
-            case 'month':
-                $periodCol = $connection->getDateFormatSql('t.period', '%Y-%m-01');
-                break;
-            default:
-                $periodCol = 't.period';
-                break;
-        }
+        $periodCol = 't.period';
 
         $columns = [
             'period' => 't.period',
@@ -79,19 +96,21 @@ class SimpleProductsAggregatedReportDataProcessor implements DataProcessorInterf
             'product_price' => 't.product_price',
         ];
 
-        if ($type == 'day') {
-            $columns['id'] = 't.id';  // to speed-up insert on duplicate key update
-        }
-
         $cols = array_keys($columns);
         $cols['total_qty'] = new \Zend_Db_Expr('SUM(t.' . $column . ')');
         $periodSubSelect->from(
             ['t' => $mainTable],
             $cols
+        )->where(
+            $periodCol . '>=?', $this->date->gmtDate('Y-m-d')
+        )->where(
+            $periodCol . '<=?', $this->date->gmtDate('Y-m-d')
+        )->where(
+            'store_id =?', $storeId
         )->group(
-            ['t.store_id', $periodCol, 't.product_id']
+            ['t.store_id', 't.product_id']
         )->order(
-            ['t.store_id', $periodCol, 'total_qty DESC']
+            ['t.store_id', 'total_qty DESC']
         );
 
         $cols = $columns;
@@ -103,21 +122,25 @@ class SimpleProductsAggregatedReportDataProcessor implements DataProcessorInterf
         $cols['prevPeriod'] = new \Zend_Db_Expr("(@prevPeriod := {$periodCol})");
         $ratingSubSelect->from($periodSubSelect, $cols);
 
-        $cols = $columns;
-        $cols['period'] = $periodCol;
-        $cols[$column] = 't.' . $column;
-        $cols['rating_pos'] = 't.rating_pos';
-        $ratingSelect->from($ratingSubSelect, $cols);
+        $cols = [];
+        $cols['store_id'] = 't.store_id as store_id';
+        $cols['product_id'] = 't.product_id as entity_id';
+        $cols['rating_pos'] = 't.rating_pos as value';
 
+
+        $attribute = $this->eavConfig->getAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            \MagentoHackathon\BestsellersSorting\Setup\InstallData::ATTRIBUTE_NAME
+        );
+
+        $ratingSelect->from($ratingSubSelect, $cols);
 
         $preQuery = "SET @pos = 0, @prevStoreId = -1, @prevPeriod = '0000-00-00'";
 
-
-
-            //$connection->query();
+        //$connection->query();
 
         //$sql = $ratingSelect->insertFromSelect($aggregationTable, array_keys($cols));
-        die($preQuery.";".$ratingSelect);
+                die($preQuery.";".$ratingSelect. PHP_EOL);
 
         //@todo use approach from \Magento\Reports\Model\ResourceModel\Helper::updateReportRatingPos
     }
