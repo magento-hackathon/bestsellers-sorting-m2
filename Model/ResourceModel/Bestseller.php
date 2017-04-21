42a6fcbe94ccc798fb658f368b8576c2e8e3dc0c
Elias Kotlyar <elias.kotlyar@mediawave.de>
2017-04-21 16:20:34 +0200

    Bestseller Index not working

--- Model/ResourceModel/Bestseller.php
+++ Model/ResourceModel/Bestseller.php
@@ -2,26 +2,28 @@
 /**
  * Bestsellers sorting of products in categories for Magento 2.
  * Copyright (C) 2016  2017 Firegento Hackathon
- * 
+ *
  * This file is part of MagentoHackathon/BestsellersSorting.
- * 
+ *
  * MagentoHackathon/BestsellersSorting is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace MagentoHackathon\BestsellersSorting\Model\ResourceModel;
 
-class Bestseller extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+use Magento\Sales\Model\ResourceModel\Report\AbstractReport;
+
+class Bestseller extends \Magento\Sales\Model\ResourceModel\Report\Bestsellers
 {
 
     /**
@@ -33,4 +35,10 @@ class Bestseller extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
     {
         $this->_init('magentohackathon_bestseller', 'bestseller_id');
     }
+
+    protected function _updateRatingPos($aggregation)
+    {
+        return $this;
+    }
+
 }
--- Model/SimpleProductsAggregatedReportDataProcessor.php
+++ Model/SimpleProductsAggregatedReportDataProcessor.php
@@ -29,114 +29,24 @@ namespace MagentoHackathon\BestsellersSorting\Model;
 
 class SimpleProductsAggregatedReportDataProcessor implements DataProcessorInterface
 {
-    /**
-     * @var \Magento\Framework\App\ResourceConnection
-     */
-    private $resource;
+
 
     /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     * @var ResourceModel\Bestseller
      */
-    private $scopeConfig;
+    private $bestseller;
 
-    /**
-     * @var \Magento\Framework\Stdlib\DateTime\DateTime
-     */
-    private $date;
-
-    /**
-     * @var \Magento\Eav\Model\Config
-     */
-    private $eavConfig;
-
-    /**
-     * SimpleProductsAggregatedReportDataProcessor constructor.
-     * @param \Magento\Framework\App\ResourceConnection $resource
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     * @param \Magento\Framework\Stdlib\DateTime\DateTime $date
-     * @param \Magento\Eav\Model\Config $eavConfig
-     */
     public function __construct(
-        \Magento\Framework\App\ResourceConnection $resource,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Framework\Stdlib\DateTime\DateTime $date,
-        \Magento\Eav\Model\Config $eavConfig
+        \MagentoHackathon\BestsellersSorting\Model\ResourceModel\Bestseller $bestseller
+
     )
     {
-        $this->resource = $resource;
-        $this->scopeConfig = $scopeConfig;
-        $this->date = $date;
-        $this->eavConfig = $eavConfig;
+
+        $this->bestseller = $bestseller;
     }
 
     public function calculate($storeId = 0)
     {
-
-        $connection = $this->resource->getConnection();
-        $type = 'month';
-        $column = 'qty_ordered';
-
-        $mainTable = $connection->getTableName('magentohackathon_bestseller');
-        $aggregationTable = $mainTable;
-
-
-        $periodSubSelect = $connection->select();
-        $ratingSubSelect = $connection->select();
-        $ratingSelect = $connection->select();
-
-        switch ($type) {
-            case 'year':
-                $periodCol = $connection->getDateFormatSql('t.period', '%Y-01-01');
-                break;
-            case 'month':
-                $periodCol = $connection->getDateFormatSql('t.period', '%Y-%m-01');
-                break;
-            default:
-                $periodCol = 't.period';
-                break;
-        }
-
-        $columns = [
-            'period' => 't.period',
-            'store_id' => 't.store_id',
-            'product_id' => 't.product_id',
-            'product_name' => 't.product_name',
-            'product_price' => 't.product_price',
-        ];
-
-        if ($type == 'day') {
-            $columns['id'] = 't.id';  // to speed-up insert on duplicate key update
-        }
-
-        $cols = array_keys($columns);
-        $cols['total_qty'] = new \Zend_Db_Expr('SUM(t.' . $column . ')');
-        $periodSubSelect->from(
-            ['t' => $mainTable],
-            $cols
-        )->group(
-            ['t.store_id', $periodCol, 't.product_id']
-        )->order(
-            ['t.store_id', $periodCol, 'total_qty DESC']
-        );
-
-        $cols = $columns;
-        $cols[$column] = 't.total_qty';
-        $cols['rating_pos'] = new \Zend_Db_Expr(
-            "(@pos := IF(t.`store_id` <> @prevStoreId OR {$periodCol} <> @prevPeriod, 1, @pos+1))"
-        );
-        $cols['prevStoreId'] = new \Zend_Db_Expr('(@prevStoreId := t.`store_id`)');
-        $cols['prevPeriod'] = new \Zend_Db_Expr("(@prevPeriod := {$periodCol})");
-        $ratingSubSelect->from($periodSubSelect, $cols);
-
-        $cols = $columns;
-        $cols['period'] = $periodCol;
-        $cols[$column] = 't.' . $column;
-        $cols['rating_pos'] = 't.rating_pos';
-        $ratingSelect->from($ratingSubSelect, $cols);
-
-        $sql = $ratingSelect->insertFromSelect($aggregationTable, array_keys($cols));
-        $connection->query("SET @pos = 0, @prevStoreId = -1, @prevPeriod = '0000-00-00'");
-        $connection->query($sql);
-        return $this;
+        $this->bestseller->aggregate(null, null);
     }
 }
